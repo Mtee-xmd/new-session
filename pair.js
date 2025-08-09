76d81b1453abe8b1025bd6cf893d6a4df8edd458
Tanaka humbah <tanakahumbah@gmail.com>
2025-08-09 23:43:16 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -3,81 +3,88 @@ const express = require('express');
 const fs = require('fs');
 let router = express.Router();
 const pino = require("pino");
-const { default: makeWASocket, useMultiFileAuthState, delay, Browsers, makeCacheableSignalKeyStore, getAggregateVotesInPollMessage, DisconnectReason, WA_DEFAULT_EPHEMERAL, jidNormalizedUser, proto, getDevice, generateWAMessageFromContent, fetchLatestBaileysVersion, makeInMemoryStore, getContentType, generateForwardMessageContent, downloadContentFromMessage, jidDecode } = require('@whiskeysockets/baileys')
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  delay,
+  Browsers,
+  makeCacheableSignalKeyStore
+} = require('@whiskeysockets/baileys');
 
 const { upload } = require('./mega');
+
 function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true });
+  if (!fs.existsSync(FilePath)) return false;
+  fs.rmSync(FilePath, { recursive: true, force: true });
 }
+
 router.get('/', async (req, res) => {
-    const id = makeid();
-    let num = req.query.number;
-    async function MACODER_V4_PAIR_CODE() {
-        const {
-            state,
-            saveCreds
-        } = await useMultiFileAuthState('./temp/' + id);
-        try {
-var items = ["Safari"];
-function selectRandomItem(array) {
-  var randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-var randomItem = selectRandomItem(items);
-            
-            let sock = makeWASocket({
-                auth: {
-                    creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
-                },
-                printQRInTerminal: false,
-                generateHighQualityLinkPreview: true,
-                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                syncFullHistory: false,
-                browser: Browsers.macOS(randomItem)
-            });
-            if (!sock.authState.creds.registered) {
-                await delay(1500);
-                num = num.replace(/[^0-9]/g, '');
-                const code = await sock.requestPairingCode(num);
-                if (!res.headersSent) {
-                    await res.send({ code });
-                }
+  const id = makeid();
+  let num = req.query.number;
+
+  async function MTEE_XMD_PAIR_CODE() {
+    const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+    try {
+      var items = ["Safari"];
+      function selectRandomItem(array) {
+        var randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+      }
+      var randomItem = selectRandomItem(items);
+
+      let sock = makeWASocket({
+        auth: {
+          creds: state.creds,
+          keys: makeCacheableSignalKeyStore(
+            state.keys,
+            pino({ level: "fatal" }).child({ level: "fatal" })
+          ),
+        },
+        printQRInTerminal: false,
+        generateHighQualityLinkPreview: true,
+        logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+        syncFullHistory: false,
+        browser: Browsers.macOS(randomItem)
+      });
+
+      if (!sock.authState.creds.registered) {
+        await delay(1500);
+        num = num.replace(/[^0-9]/g, '');
+        const code = await sock.requestPairingCode(num);
+        if (!res.headersSent) {
+          await res.send({ code });
+        }
+      }
+
+      sock.ev.on('creds.update', saveCreds);
+
+      sock.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect } = s;
+
+        if (connection == "open") {
+          await delay(5000);
+          let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+          let rf = __dirname + `/temp/${id}/creds.json`;
+
+          function generateRandomText() {
+            const prefix = "3EB";
+            const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            let randomText = prefix;
+            for (let i = prefix.length; i < 22; i++) {
+              const randomIndex = Math.floor(Math.random() * characters.length);
+              randomText += characters.charAt(randomIndex);
             }
-            sock.ev.on('creds.update', saveCreds);
-            sock.ev.on("connection.update", async (s) => {
+            return randomText;
+          }
+          const randomText = generateRandomText();
 
-    const {
-                    connection,
-                    lastDisconnect
-                } = s;
-                
-                if (connection == "open") {
-                    await delay(5000);
-                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                    let rf = __dirname + `/temp/${id}/creds.json`;
-                    function generateRandomText() {
-                        const prefix = "3EB";
-                        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                        let randomText = prefix;
-                        for (let i = prefix.length; i < 22; i++) {
-                            const randomIndex = Math.floor(Math.random() * characters.length);
-                            randomText += characters.charAt(randomIndex);
-                        }
-                        return randomText;
-                    }
-                    const randomText = generateRandomText();
-                    try {
+          try {
+            const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
+            const string_session = mega_url.replace('https://mega.nz/file/', '');
+            let md = "MTEE-XMD=" + string_session;
+            let code = await sock.sendMessage(sock.user.id, { text: md });
 
-
-                        
-                        const { upload } = require('./mega');
-                        const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
-                        const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "MTEE-XMD=" + string_session;
-                        let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, MTEE-XMD User!* 👋🏻
+            let desc = `*Hey there, MTEE-XMD User!* 👋🏻
 
 Thanks for using *MTEE-XMD* — your session has been successfully created!
 
@@ -97,23 +104,24 @@ https://github.com/Mtee-xmd/MTEE-XMD
 ——————
 
 > *© Powered by Tkay*
-Stay cool and hack smart. ✌🏻`; 
-                        await sock.sendMessage(sock.user.id, {
-text: desc,
-contextInfo: {
-externalAdReply: {
-title: "MTEE-XMD",
-thumbnailUrl: "https://files.catbox.moe/y1mwzc.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vb6EJfCHLHQQGd2KGL1P",
-mediaType: 1,
-renderLargerThumbnail: true
-}  
-}
-},
-{quoted:code })
-                    } catch (e) {
-                            let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `Hey there, MTEE-XMD User!* 👋🏻
+Stay cool and hack smart. ✌🏻`;
+
+            await sock.sendMessage(sock.user.id, {
+              text: desc,
+              contextInfo: {
+                externalAdReply: {
+                  title: "MTEE-XMD",
+                  thumbnailUrl: "https://files.catbox.moe/y1mwzc.jpg",
+                  sourceUrl: "https://whatsapp.com/channel/0029Vb6EJfCHLHQQGd2KGL1P",
+                  mediaType: 1,
+                  renderLargerThumbnail: true
+                }
+              }
+            }, { quoted: code });
+
+          } catch (e) {
+            let ddd = sock.sendMessage(sock.user.id, { text: e });
+            let desc = `Hey there, MTEE-XMD User!* 👋🏻
 
 Thanks for using *MTEE-XMD* — your session has been successfully created!
 
@@ -134,44 +142,42 @@ https://github.com/Mtee-xmd/MTEE-XMD
 
 > *© Powered by tkay*
 Stay cool and hack smart. ✌🏻`;
-                            await sock.sendMessage(sock.user.id, {
-text: desc,
-contextInfo: {
-externalAdReply: {
-title: "bleurainz",
-thumbnailUrl: "https://files.catbox.moe/y1mwzc.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vb6EJfCHLHQQGd2KGL1P",
-mediaType: 2,
-renderLargerThumbnail: true,
-showAdAttribution: true
-}  
-}
-},
-{quoted:ddd })
-                    }
-                    await delay(10);
-                    await sock.ws.close();
-                    await removeFile('./temp/' + id);
-                    console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ✅ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...`);
-                    await delay(10);
-                    process.exit();
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                    await delay(10);
-                    PRINCESS_V4_PAIR_CODE();
+
+            await sock.sendMessage(sock.user.id, {
+              text: desc,
+              contextInfo: {
+                externalAdReply: {
+                  title: "bleurainz",
+                  thumbnailUrl: "https://files.catbox.moe/y1mwzc.jpg",
+                  sourceUrl: "https://whatsapp.com/channel/0029Vb6EJfCHLHQQGd2KGL1P",
+                  mediaType: 2,
+                  renderLargerThumbnail: true,
+                  showAdAttribution: true
                 }
-            });
-        } catch (err) {
-            console.log("service restated");
-            await removeFile('./temp/' + id);
-            if (!res.headersSent) {
-                await res.send({ code: "❗ Service Unavailable" });
-            }
+              }
+            }, { quoted: ddd });
+          }
+
+          await delay(10);
+          await sock.ws.close();
+          await removeFile('./temp/' + id);
+          console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ✅ Process completed, server continues running...`);
+
+        } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+          await delay(10);
+          MTEE_XMD_PAIR_CODE();
         }
+      });
+    } catch (err) {
+      console.log("service restarted due to error:", err);
+      await removeFile('./temp/' + id);
+      if (!res.headersSent) {
+        await res.send({ code: "❗ Service Unavailable" });
+      }
     }
-   return await PRINCESS_V4_PAIR_CODE();
-});/*
-setInterval(() => {
-    console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
-    process.exit();
-}, 180000); //30min*/
+  }
+
+  return await MTEE_XMD_PAIR_CODE();
+});
+
 module.exports = router;
